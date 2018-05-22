3b421d4af116a9740b6180bae75e7550dd30a88f
sili114 <39498974+sili114@users.noreply.github.com>
2018-05-22 19:46:09 +0200

    Začetne definicije
    Začel delati model za igro.

--- model less.py
+++ model less.py
@@ -1,1 +1,22 @@
+BELA, CRNA = 'bela', 'crna'
 
+class Figura:
+
+    def __init__(self, x, y, barva):
+        self.x = x
+        self.y = y
+        self.stevec = 0
+        self.barva = barva
+
+    def prestavi_figuro(self, x, y):
+        if je_veljavna_poteza(self, x, y):
+            self.x = x
+            self.y = y
+            self.stevec += 1
+            if self.stevec % 3 == 0:
+
+
+
+    def je_veljavna_poteza(self, x, y):
+        if 0 <= x < 7 and 0 <= y < 7:
+            
