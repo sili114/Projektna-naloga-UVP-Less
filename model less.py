6509489d50dad52e859450ef5badf776dfc2162b
sili114 <39498974+sili114@users.noreply.github.com>
2018-07-24 11:01:06 +0200

    Dodal ploščo
    Dodal razred plošča in ustrezno oblikovanje le tega.

--- model less.py
+++ model less.py
@@ -1,3 +1,48 @@
+class Plosca:
+
+    def __init__(self, visina=6, sirina=6):
+        self.visina = visina
+        self.sirina = sirina
+        self.poteze = 0
+        self.belefigure = [(1,1), (1,2), (2,1), (2,2)]
+        self.crnefigure = [(5,5), (5,6), (6,5), (6,6)]
+
+    def __repr__(self):
+        return 'Plosca(visina={}, sirina={}, belefigure={}, crnefigure={})'.format(
+            self.visina, self.sirina, self.belefigure, self.crnefigure
+        )
+
+    def __str__(self):
+        polja = []
+        for _ in range(self.visina):
+            polja.append(self.sirina * [' '])
+        for vrstica, stolpec in self.belefigure:
+            polja[vrstica][stolpec] = 'O'
+        for vrstica, stolpec in self.crnefigure:
+            polja[vrstica][stolpec] = 'X'
+
+        niz = ''
+        rob = '+' + self.sirina * '-' + '+\n'
+        for vrstica in polja:
+            niz += '|' + ''.join(vrstica) + '|\n'
+        return rob + niz + rob
+
+    def prestavi_figuro(self, x, y):
+        if (x, y) & self.zasedena_polja
+
+
+    def zasedena_polja(self):
+        zasedena = set()
+        for figura in self.belefigure:
+            zasedena.update(figura.zasedena_polja())
+        for figura in self.crnefigure:
+            zasedena.update(figura.zasedena_polja())
+        return zasedena
+
+
+
+
+
 BELA, CRNA = 'bela', 'crna'
 
 class Figura:
@@ -5,7 +50,6 @@ class Figura:
     def __init__(self, x, y, barva):
         self.x = x
         self.y = y
-        self.stevec = 0
         self.barva = barva
 
     def __str__(self):
@@ -32,5 +76,3 @@ class Figura:
 
     def je_na_potezi(self):
         return self.barva == BARVA
-
-    
